04c61b24820566fdff3a651e14eae817ef84b020
lenovo <frqhero@gmail.com>
2023-11-01 23:42:34 +0600

    Complete a couple of steps

--- start_over.py
+++ start_over.py
@@ -10,44 +10,90 @@ async def go_to_sleep(seconds):
         await asyncio.sleep(0)
 
 
-async def blink(canvas, row, column, symbol='*'):
+async def blink(canvas, row, column, symbol='*', offset=1):
     while True:
-        canvas.addstr(row, column, symbol, curses.A_DIM)
-        await go_to_sleep(2)
+        if offset == 0:
+            canvas.addstr(row, column, symbol, curses.A_DIM)
+            await go_to_sleep(2)
+            offset += 1
 
-        canvas.addstr(row, column, symbol)
-        await go_to_sleep(0.3)
+        if offset == 1:
+            canvas.addstr(row, column, symbol)
+            await go_to_sleep(0.3)
+            offset += 1
 
-        canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await go_to_sleep(0.5)
+        if offset == 2:
+            canvas.addstr(row, column, symbol, curses.A_BOLD)
+            await go_to_sleep(0.5)
+            offset += 1
 
-        canvas.addstr(row, column, symbol)
-        await go_to_sleep(0.3)
+        if offset == 3:
+            canvas.addstr(row, column, symbol)
+            await go_to_sleep(0.3)
+            offset = 0
+
+
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
 
 
 def draw(canvas):
-    row, column = (5, 20)
     curses.curs_set(False)
     canvas.border()
+    canvas.nodelay(True)
 
-    canvas_height, canvas_width = canvas.getmaxyx()
-    print(canvas_height, canvas_width)
+    height, width = canvas.getmaxyx()
     symbols = '+*.:'
 
     coroutines = [
         blink(
             canvas,
-            randint(1, canvas_height - 2),
-            randint(1, canvas_width - 2),
+            randint(1, height - 2),
+            randint(1, width - 2),
             choice(symbols),
+            randint(0, 3)
         )
-        for _ in range(3)
+        for _ in range(100)
     ]
 
+    fire_coro = fire(canvas, height - 2, width / 2)
+    coroutines.append(fire_coro)
+
     while True:
-        for my_coroutine in coroutines:
-            my_coroutine.send(None)
-            canvas.refresh()
+        for coroutine in coroutines.copy():
+            try:
+                coroutine.send(None)
+                canvas.refresh()
+            except StopIteration:
+                coroutines.remove(coroutine)
+        if len(coroutines) == 0:
+            break
         time.sleep(0.1)
 
 
