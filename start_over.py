4fad978b4ad47104de8586a4411a931f9bdc99b8
lenovo <frqhero@gmail.com>
2023-11-03 02:31:46 +0600

    Animate the rocket

--- start_over.py
+++ start_over.py
@@ -1,5 +1,6 @@
 import asyncio
 from random import choice, randint
+from itertools import cycle
 import time
 import curses
 
@@ -63,6 +64,53 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
         column += columns_speed
 
 
+def load_frame_from_file(filename):
+    with open(filename, 'r') as fd:
+        return fd.read()
+    
+
+def draw_frame(canvas, start_row, start_column, text, negative=False):
+    """Draw multiline text fragment on canvas, erase text instead of drawing if negative=True is specified."""
+    
+    rows_number, columns_number = canvas.getmaxyx()
+
+    for row, line in enumerate(text.splitlines(), round(start_row)):
+        if row < 0:
+            continue
+
+        if row >= rows_number:
+            break
+
+        for column, symbol in enumerate(line, round(start_column)):
+            if column < 0:
+                continue
+
+            if column >= columns_number:
+                break
+                
+            if symbol == ' ':
+                continue
+
+            # Check that current position it is not in a lower right corner of the window
+            # Curses will raise exception in that case. Don`t ask why…
+            # https://docs.python.org/3/library/curses.html#curses.window.addch
+            if row == rows_number - 1 and column == columns_number - 1:
+                continue
+
+            symbol = symbol if not negative else ' '
+            canvas.addch(row, column, symbol)
+
+
+async def animate_frames(canvas, start_row, start_column, frames):
+    frames_gen = cycle(frames)
+
+    while True:
+        current_frame = next(frames_gen)
+        draw_frame(canvas, start_row, start_column, current_frame)
+        await go_to_sleep(0.3)
+        draw_frame(canvas, start_row, start_column, current_frame, negative=True)
+
+
 def draw(canvas):
     curses.curs_set(False)
     canvas.border()
@@ -79,11 +127,25 @@ def draw(canvas):
             choice(symbols),
             randint(0, 3)
         )
-        for _ in range(100)
+        for _ in range(50)
     ]
 
     fire_coro = fire(canvas, height - 2, width / 2)
     coroutines.append(fire_coro)
+
+    rocket_frame_1 = load_frame_from_file(
+        'frames/rocket_frame_1.txt'
+    )
+    rocket_frame_2 = load_frame_from_file(
+        'frames/rocket_frame_2.txt'
+    )
+
+    rocket_frames = (rocket_frame_1, rocket_frame_2)
+    # draw_frame(canvas, 1, 10, rocket_frame_1)
+    # canvas.refresh()
+
+    rocket_coro = animate_frames(canvas, 1, 10, rocket_frames)
+    coroutines.append(rocket_coro)
 
     while True:
         for coroutine in coroutines.copy():
